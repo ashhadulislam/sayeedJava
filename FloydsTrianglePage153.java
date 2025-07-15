06a8e0b39b23cee1ced8a2bcfc150f17989885ff
Ashhad <ashhadulislam@gmail.com>
2025-07-15 16:23:29 +0530

    Create FloydsTrianglePage153.java

---
+++ FloydsTrianglePage153.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+       
+       int start=1;
+       for(int i=1;i<=5;i++){
+           for(int j=0;j<i;j++){
+                 System.out.print( start +" " );
+                 start++ ;
+            }
+       System.out.println();
+       }
+    }
+}
+
+
+
