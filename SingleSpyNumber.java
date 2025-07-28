919c83651e40a2041956fec5b2fd046463f56e6d
Ashhad <ashhadulislam@gmail.com>
2025-07-28 17:06:25 +0530

    Create SingleSpyNumber.java

---
+++ SingleSpyNumber.java
@@ -0,0 +1,40 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.*;
+class Main {
+     public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        int ld,sum=0;
+        float pdt=1;
+        System.out.println("Enter the number:");
+        int n=in.nextInt();
+        int n1=n;
+        while(n>0)
+        {
+            ld=n%10;
+            n=n/10;
+            sum=sum+ld;
+            pdt=pdt*ld;
+        }
+        if(sum==pdt)
+        {
+            System.out.println(n1+" is a spy number");
+        }
+        else
+        {
+            System.out.println(n1+" is not a spy number");
+        }
+    }
+}
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
